845022556e8c7c5a579aa31d4cebcb79d3f72839
月识 <141123918+infileap@users.noreply.github.com>
2026-09-02 18:06:48 +0800

    Add bytecode capture metadata probe

---
+++ src/compiler/bytecode_capture_probe.c
@@ -0,0 +1,16 @@
+#include "bytecode.h"
+#include <assert.h>
+#include <string.h>
+
+int main(void) {
+    Bytecode bc;
+    bytecode_init(&bc);
+    assert(bytecode_add_capture(&bc, "base") == 0);
+    assert(bytecode_add_capture(&bc, "offset") == 1);
+    assert(bc.capture_count == 2);
+    assert(strcmp(bc.capture_names[0], "base") == 0);
+    assert(strcmp(bc.capture_names[1], "offset") == 0);
+    assert(bytecode_add_capture(&bc, NULL) == -1);
+    bytecode_free(&bc);
+    return 0;
+}
